e3bbb8afbef57c07355686a3ac94ebe58bedf5c5
Oh-Nahyun <annieonh@gmail.com>
2024-06-29 21:24:34 +0900

    Test_05_AI [3]
    Game Title, Loading Scene 추가

---
+++ Assets/Scripts/UI/GameStartButton.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameStartButton : MonoBehaviour
+{
+    /// <summary>
+    /// 다음에 타이틀씬이 끝나고 나서 불려질 씬의 이름
+    /// </summary>
+    public string nextSceneName = "Loading";
+
+    /// <summary>
+    /// [GAME START] 버튼
+    /// </summary>
+    Button startButton;
+
+    private void Awake()
+    {
+        startButton = GetComponent<Button>();
+
+        // [GAME START] 버튼이 눌려지면 AddListener로 등록한 함수 실행
+        startButton.onClick.AddListener(() =>
+        {
+            LoadScene();
+        });
+    }
+
+    /// <summary>
+    /// 다음 씬을 로드하는 함수
+    /// </summary>
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
